4cfe416cd4f6399ada3ac57ca094f28a5afa83eb
“Juanes23132A“ <bantees2005@gmail.com>
2024-10-21 09:41:00 -0500

    correccion de error en Reservas al guardar fecha

--- src/domain/models/reservas.ts
+++ src/domain/models/reservas.ts
@@ -3,7 +3,7 @@ export class Reservas {
     id?: number;
     usuario_id: number;
     vehiculo_id: number;
-    fecha_reserva: Date = new  Date();
+    fecha_reserva: Date;
 
 
     constructor(infoReservas: {
@@ -15,5 +15,6 @@ export class Reservas {
         this.id = infoReservas.id
         this.usuario_id = infoReservas.usuario_id
         this.vehiculo_id = infoReservas.vehiculo_id
+        this.fecha_reserva = infoReservas.fecha_reserva
     }
 }
